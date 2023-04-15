aa0abbbe933c6575701539257d796fe1e4a2b7c6
slametsampon <slamet@pon.co.id>
2023-04-16 06:20:28 +0700

    take out layoutWrapper

--- pages/_app.js
+++ pages/_app.js
@@ -10,9 +10,7 @@ export default function App({ Component, pageProps}) {
         <Head>
           <meta content="width=device-width, initial-scale=1" name="viewport" />
         </Head>
-        <LayoutWrapper>
             <Component {...pageProps} />
-        </LayoutWrapper>
       </ThemeProvider>
   )
 }
